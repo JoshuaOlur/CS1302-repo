c8d05fed4adfd6b2c2ab63df9eee4d610035a529
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-09-18 16:32:51 -0400

    Tests for Substotal
    Created Junit test cases for the subtotal method.

--- code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetSubtotal.java
+++ code/Bill/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestGetSubtotal.java
@@ -1,14 +1,37 @@
 package edu.westga.cs1302.bill.test.model.bill_calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestGetSubtotal {
+import edu.westga.cs1302.bill.model.BillCalculator;
+import edu.westga.cs1302.bill.model.BillItem;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+ class TestGetSubtotal {
 
+    private BillItem item1;
+    private BillItem item2;
+    private BillItem item3;
+    private BillItem[] items;
+
+    @BeforeEach
+     void setUp() {
+        this.item1 = new BillItem("Item 1", 10.0);
+        this.item2 = new BillItem("Item 2", 20.0);
+        this.item3 = new BillItem("Item 3", 30.0);
+        this.items = new BillItem[] { item1, item2, item3 };
+    }
+
+    @Test
+     void testGetSubTotal() {
+        double result = BillCalculator.getSubTotal(this.items);
+        assertEquals(60.0, result, 0.001);
+    }
+
+    @Test
+     void testGetSubTotalWithNullItem() {
+        BillItem[] itemsWithNull = new BillItem[] { item1, null, item3 };
+        double result = BillCalculator.getSubTotal(itemsWithNull);
+        assertEquals(40.0, result, 0.001);
+    }
 }
