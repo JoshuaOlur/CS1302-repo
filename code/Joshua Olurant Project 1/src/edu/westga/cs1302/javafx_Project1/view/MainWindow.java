5c91068c5babed909c304302ba42fe2b4d1f641f
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-09-23 16:40:47 -0400

    Starting Project
    Putting this commit as a note of when I started the project.

---
+++ code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/view/MainWindow.java
@@ -0,0 +1,10 @@
+package edu.westga.cs1302.javafx_Project1.view;
+
+/** Codebehind for the Main Window of the application.
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class MainWindow {
+
+}
