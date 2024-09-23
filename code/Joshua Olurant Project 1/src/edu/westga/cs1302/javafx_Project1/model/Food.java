e86841101008e19abe6b3b4de9c6307c84df8c8d
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-09-23 18:48:47 -0400

    Add getter methods for name, type, and quantity
    Added methods to retrieve the name, type, and quantity of the food.

--- code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/model/Food.java
+++ code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/model/Food.java
@@ -10,4 +10,17 @@ public class Food {
 		this.type = type;
 		this.quantity = 0;
 	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public int getQuantity() {
+		return this.quantity;
+	}
+
 }
