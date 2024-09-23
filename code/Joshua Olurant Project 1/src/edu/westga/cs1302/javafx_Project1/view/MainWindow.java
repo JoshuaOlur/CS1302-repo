349ca6c85ffa1f12b9349007d8158397d0ac41a6
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-09-23 17:39:09 -0400

    Add TextField for food name input
    Added a label and a TextField to allow the user to enter the name of a new food item.

--- code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/view/MainWindow.java
+++ code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/view/MainWindow.java
@@ -1,10 +1,17 @@
 package edu.westga.cs1302.javafx_Project1.view;
 
-/** Codebehind for the Main Window of the application.
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Codebehind for the Main Window of the application.
  * 
  * @author CS 1302
  * @version Fall 2024
  */
 public class MainWindow {
+
+	@FXML
+	private TextField foodNameTextField;
 
 }
