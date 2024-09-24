e3c4b2aed130dbed55f981559c7be6a2354e8867
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-09-24 17:13:54 -0400

    Add static method to calculate total food quantity
    Added a static method  in the utility class that sums the quantity of all food items in the pantry.

--- code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/utils/PantryUtility.java
+++ code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/utils/PantryUtility.java
@@ -1,5 +1,16 @@
 package edu.westga.cs1302.javafx_Project1.utils;
 
+import java.util.List;
+
+import edu.westga.cs1302.javafx_Project1.model.Food;
+
 public class PantryUtility {
 
+	public static int getTotalQuantity(List<Food> pantryItems) {
+        int totalQuantity = 0;
+        for (Food food : pantryItems) {
+            totalQuantity += food.getQuantity();
+        }
+        return totalQuantity;
+    }
 }
