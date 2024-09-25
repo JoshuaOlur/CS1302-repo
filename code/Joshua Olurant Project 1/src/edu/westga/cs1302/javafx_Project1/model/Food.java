236f1723100717ebfcbcd6c147904a15bb7eb3e9
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-09-25 14:18:52 -0400

    Checkstyle
    Went back to fix checkstyle errors and anything not up to proper standards.

--- code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/model/Food.java
+++ code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/model/Food.java
@@ -1,44 +1,79 @@
 package edu.westga.cs1302.javafx_Project1.model;
 
+/**
+ * Represents a food item with a name, type, and quantity.
+ * The quantity of the food item can be modified, but the name and type are immutable.
+ * 
+ * @author Joshua Oluranti
+ * @version Fall 2024
+ */
 public class Food {
-	private final String name;
-	private final String type;
-	private int quantity;
+    private final String name;
+    private final String type;
+    private int quantity;
 
-	public Food(String name, String type) {
-		this.name = name;
-		this.type = type;
-		this.quantity = 0;
-	}
+    /**
+     * Creates a new Food object with the specified name and type.
+     * The quantity is initialized to 0.
+     * 
+     * @param name the name of the food item
+     * @param type the type of the food item
+     */
+    public Food(String name, String type) {
+        this.name = name;
+        this.type = type;
+        this.quantity = 0;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * Gets the name of the food item.
+     * 
+     * @return the name of the food item
+     */
+    public String getName() {
+        return this.name;
+    }
 
-	public String getType() {
-		return this.type;
-	}
+    /**
+     * Gets the type of the food item.
+     * 
+     * @return the type of the food item
+     */
+    public String getType() {
+        return this.type;
+    }
 
-	public int getQuantity() {
-		return this.quantity;
-	}
+    /**
+     * Gets the current quantity of the food item.
+     * 
+     * @return the quantity of the food item
+     */
+    public int getQuantity() {
+        return this.quantity;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	
-	public void incrementQuantity() {
-	    this.quantity++;
-	}
-	
-	public void decrementQuantity() {
-	    if (this.quantity > 0) {
-	        this.quantity--;
-	    }
-	}
-
-	@Override
-	public String toString() {
-		return this.name + " – " + this.quantity;
-	}
+    /**
+     * Sets the quantity of the food item.
+     * 
+     * @param quantity the new quantity of the food item
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    /**
+     * Increments the quantity of the food item by 1.
+     */
+    public void incrementQuantity() {
+        this.quantity++;
+    }
+    
+    /**
+     * Decrements the quantity of the food item by 1, if the quantity is greater than 0.
+     */
+    public void decrementQuantity() {
+        if (this.quantity > 0) {
+            this.quantity--;
+        }
+    }
 }
--- code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/utils/PantryUtility.java
+++ code/Joshua Olurant Project 1/src/edu/westga/cs1302/javafx_Project1/utils/PantryUtility.java
@@ -1,12 +1,24 @@
 package edu.westga.cs1302.javafx_Project1.utils;
 
 import java.util.List;
-
 import edu.westga.cs1302.javafx_Project1.model.Food;
 
+/**
+ * Utility class for pantry related operations.
+ * Provides  the method for calculating total quantity of food items in the pantry.
+ * 
+ * @author Joshua Oluranti
+ * @version Fall 2024
+ */
 public class PantryUtility {
 
-	public static int getTotalQuantity(List<Food> pantryItems) {
+    /**
+     * Calculates the total quantity of all food items in the pantry.
+     * 
+     * @param pantryItems the list of food items in the pantry
+     * @return the total quantity of all food items
+     */
+    public static int getTotalQuantity(List<Food> pantryItems) {
         int totalQuantity = 0;
         for (Food food : pantryItems) {
             totalQuantity += food.getQuantity();
