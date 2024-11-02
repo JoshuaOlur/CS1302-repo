58d8cedac42990b4c96b01b877b3e91f9318f380
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-11-02 14:09:28 -0400

    3.B – Update Business Logic to
    added static method in RecipeConverter to convert a list of recipes to a string.

--- code/Project 2/src/edu/westga/cs1302/project2/model/RecipeConverter.java
+++ code/Project 2/src/edu/westga/cs1302/project2/model/RecipeConverter.java
@@ -1,5 +1,7 @@
 package edu.westga.cs1302.project2.model;
 
+import java.util.List;
+
 public class RecipeConverter {
 	
 	public static String convertRecipeToString(Recipe recipe) {
@@ -9,4 +11,12 @@ public class RecipeConverter {
 		}
 		return recipe.getName() + "\n" + ingredientsList.toString().replaceAll(", $", "");
 	}
+    public static String convertRecipeListToString(List<Recipe> recipes) {
+        StringBuilder allRecipesString = new StringBuilder();
+        for (Recipe recipe : recipes) {
+            allRecipesString.append(convertRecipeToString(recipe)).append("\n\n");
+        }
+        return allRecipesString.toString().trim();
+    }
 }
+
