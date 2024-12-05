53cd73923cd228c5ff85e971297d63050972eff6
Joshua O Oyebode-Oluranti <jo00087@my.westga.edu>
2024-12-05 15:30:54 -0500

    Testing Task
    Created class to test functionality of toString method in Task class.

--- code/Project 3/src/edu/westga/cs1302/project3/test/Task/TestTaskToString.java
+++ code/Project 3/src/edu/westga/cs1302/project3/test/Task/TestTaskToString.java
@@ -1,5 +1,35 @@
 package edu.westga.cs1302.project3.test.Task;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.project3.model.Task;
+
 public class TestTaskToString {
 
+    @Test
+    void testToStringWithValidTitle() {
+        Task task = new Task("Study", "Prepare for the exam");
+        assertEquals("Task: Study", task.toString());
+    }
+
+    @Test
+    void testToStringWithEmptyDescription() {
+        Task task = new Task("Read", "Read the assigned chapters");
+        assertEquals("Task: Read", task.toString());
+    }
+
+    @Test
+    void testToStringWithLongTitle() {
+        Task task = new Task("Complete JavaFX Assignment", "Finish all pending tasks for Project 3");
+        assertEquals("Task: Complete JavaFX Assignment", task.toString());
+    }
+
+    @Test
+    void testToStringWithSpecialCharactersInTitle() {
+        Task task = new Task("Clean & Organize!", "Organize the bookshelf and clean the table");
+        assertEquals("Task: Clean & Organize!", task.toString());
+    }
 }
+
